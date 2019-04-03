9af5bc417af6a766c6cab56f246adf0d9945611c
Steven Ng <steven@nomadicoder.com>
2019-04-03 11:16:04 -0400

    Remove unneded fields
    - Also, handle filenaming

--- json2csv.rb
+++ json2csv.rb
@@ -2,25 +2,30 @@
 
 require 'csv'
 require 'json'
-require 'pry'
 
-source_filename = ARGV[0]
-target_filename = ARGV[1]
+source_filename = ARGV[0] || "has_diamond_non-premanent_links_1.json"
+
+target_filename = File.basename(source_filename, ".json") + ".csv" # => "has_diamond_non-premanent_links_1.csv"
 
 json_doc = JSON.parse(File.open(source_filename).read)
 hash_docs = json_doc["response"]["docs"]
 
+exclude = ["description_display", "subject_topic_facet", "language_facet", "score"]
+
 # Get all of the possible keys
-keys = hash_docs.map { |hash| hash.keys }.flatten.uniq!
+keys = hash_docs.map { |hash| hash.keys }.flatten.uniq!  # => 
+keys.reject! { |k| exclude.include?(k) }                 # => 
 
 # Generate the CSV string
-csv_string = CSV.generate(headers: true) do |csv|
+CSV.open(target_filename, "w", headers: true) do |csv|
   csv << keys
   JSON.parse(File.open(source_filename).read).each do |hash|
     hash_docs.each do |hash|
-      csv << keys.map { |k| hash.fetch(k, "") }
+      csv << keys.map { |k|
+        value = hash.fetch(k, "")
+        value
+      }
     end
   end
 end
 
-puts csv_string
